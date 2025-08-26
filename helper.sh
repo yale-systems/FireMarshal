ec93a34b51b2833fc09ac26bea3fa31874fc9190
Amirmohammad <amirm.nazari98@gmail.com>
2025-08-25 22:10:26 -0400

    fixing workload for fpga prototype

--- helper.sh
+++ helper.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -x
-cd boards/default/lib/ && make static; cd ../../..
+
+# Building drivers
 cd boards/default/drivers/accnet-driver/ && make; cd ../../../..
 cd boards/default/drivers/iocache-driver/ && make; cd ../../../..
-./marshal -v clean br-base.json
-./marshal -v build br-base.json
-cp images/firechip/br-base/br-base.img /usr/local/cad/an683/sim-dir/sim_slot_0/br-base0-br-base.img
-cp images/firechip/br-base/br-base-bin /usr/local/cad/an683/sim-dir/sim_slot_0/br-base0-br-base-bin
+
+# Building userspace codes and placing them into the filesystem
+cd boards/default/lib/ && make static; cd ../../..
+cp boards/default/lib/file_receiver boards/prototype/base-workloads/br-new/overlay/root/
+# cp boards/default/lib/udp_client_kernel boards/prototype/base-workloads/br-new/overlay/bin/
+# cp boards/default/lib/interrupt_exp  boards/prototype/base-workloads/br-new/overlay/root/
+
+# Build image
+./marshal -v -d clean br-new.json
+./marshal -v -d build br-new.json
+./marshal -v -d install -t prototype br-new.json
+
 set +x
