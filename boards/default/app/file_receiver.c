3b09bb70fbae23b677e3d249cf9b1d876f67f96f
Amirmohammad <amirm.nazari98@gmail.com>
2025-08-25 22:17:58 -0400

    make app always receive files

--- boards/default/app/file_receiver.c
+++ boards/default/app/file_receiver.c
@@ -29,61 +29,63 @@ int main() {
         exit(1);
     }
 
-    // Listen
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed");
-        exit(1);
+    for (;;) {
+        // Listen
+        if (listen(server_fd, 3) < 0) {
+            perror("listen failed");
+            exit(1);
+        }
+        printf("Server listening on port %d...\n", PORT);
+
+        // Accept
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                (socklen_t *)&addrlen)) < 0) {
+            perror("accept failed");
+            exit(1);
+        }
+
+        // --- Receive filename first ---
+        uint32_t fname_len;
+        if (recv(new_socket, &fname_len, sizeof(fname_len), 0) <= 0) {
+            perror("recv filename length failed");
+            close(new_socket);
+            return 1;
+        }
+        fname_len = ntohl(fname_len); // convert from network byte order
+
+        char filename[256];
+        if (fname_len >= sizeof(filename)) {
+            fprintf(stderr, "Filename too long!\n");
+            close(new_socket);
+            return 1;
+        }
+
+        if (recv(new_socket, filename, fname_len, 0) <= 0) {
+            perror("recv filename failed");
+            close(new_socket);
+            return 1;
+        }
+        filename[fname_len] = '\0'; // null terminate
+
+        printf("Receiving file: %s\n", filename);
+
+        // Open output file with same name
+        fp = fopen(filename, "wb");
+        if (fp == NULL) {
+            perror("File open failed");
+            close(new_socket);
+            return 1;
+        }
+
+        // --- Receive file content ---
+        ssize_t n;
+        while ((n = recv(new_socket, buffer, BUF_SIZE, 0)) > 0) {
+            fwrite(buffer, 1, n, fp);
+        }
+
+        printf("File '%s' received successfully.\n", filename);
+        fclose(fp);
     }
-    printf("Server listening on port %d...\n", PORT);
-
-    // Accept
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0) {
-        perror("accept failed");
-        exit(1);
-    }
-
-    // --- Receive filename first ---
-    uint32_t fname_len;
-    if (recv(new_socket, &fname_len, sizeof(fname_len), 0) <= 0) {
-        perror("recv filename length failed");
-        close(new_socket);
-        return 1;
-    }
-    fname_len = ntohl(fname_len); // convert from network byte order
-
-    char filename[256];
-    if (fname_len >= sizeof(filename)) {
-        fprintf(stderr, "Filename too long!\n");
-        close(new_socket);
-        return 1;
-    }
-
-    if (recv(new_socket, filename, fname_len, 0) <= 0) {
-        perror("recv filename failed");
-        close(new_socket);
-        return 1;
-    }
-    filename[fname_len] = '\0'; // null terminate
-
-    printf("Receiving file: %s\n", filename);
-
-    // Open output file with same name
-    fp = fopen(filename, "wb");
-    if (fp == NULL) {
-        perror("File open failed");
-        close(new_socket);
-        return 1;
-    }
-
-    // --- Receive file content ---
-    ssize_t n;
-    while ((n = recv(new_socket, buffer, BUF_SIZE, 0)) > 0) {
-        fwrite(buffer, 1, n, fp);
-    }
-
-    printf("File '%s' received successfully.\n", filename);
-    fclose(fp);
     close(new_socket);
     close(server_fd);
     return 0;
