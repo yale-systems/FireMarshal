93ed1256336b9f7d1e6538f50d537e99dfbf2418
Amirmohammad <amirm.nazari98@gmail.com>
2025-08-25 23:38:42 -0400

    changing output format of udp app

--- boards/default/lib/udp_exp.c
+++ boards/default/lib/udp_exp.c
@@ -44,8 +44,6 @@ int main(int argc, char **argv) {
     int n_tests = 64;
     bool debug = false;
     uint32_t payload_size = 32*1024;
-    long total = 0;
-    double avg = 0;
     char *src_ip = "10.0.0.2";
     char *src_mac = "0c:42:a1:a8:2d:e6";
     uint16_t src_port = 1111;
@@ -152,6 +150,8 @@ int main(int argc, char **argv) {
     /* Init rings */
     accnet_start_ring(accnet);
 
+    long long sum_ns = 0, min_ns = 0, max_ns = 0;
+
     /* Run Test */
     for (int i = 0; i < n_tests; i++) {
         struct timespec diff;
@@ -161,13 +161,19 @@ int main(int argc, char **argv) {
         else if (strcmp(mode, MODE_BLOCKING) == 0) {
             diff = test_udp_latency_block(accnet, iocache, payload, payload_size, debug);
         }
-
-        long time_diff = diff.tv_sec * 1e9 + diff.tv_nsec;
-        total += time_diff;
-        printf("*** Trial %d: %ld ns\n", i, time_diff);
+        long long rtt_ns = diff.tv_sec * 1e9 + diff.tv_nsec;
+        if (i == 0) {
+            min_ns = max_ns = rtt_ns;
+        } else {
+            if (rtt_ns < min_ns) min_ns = rtt_ns;
+            if (rtt_ns > max_ns) max_ns = rtt_ns;
+        }
+        sum_ns += rtt_ns;
+        printf("iter=%d rtt=%.3f us\n", i, rtt_ns / 1e3);
     }
-    avg = (double) total / n_tests;
-    printf("*** Overall (%s):\nAverage delay: %0.2f ns\n", mode, avg);
+    double avg_us = (sum_ns / (double)n_tests) / 1e3;
+    printf("\nResults (%s): recv=%d  min=%.3f us  avg=%.3f us  max=%.3f us\n",
+                mode, n_tests, min_ns/1e3, avg_us, max_ns/1e3);
 
     /* Close Accnet */
     accnet_close(accnet);
